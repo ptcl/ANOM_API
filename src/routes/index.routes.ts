15f79110faafd301622a8b4af023f9951622bba5
Lucas <lucas.raffalli17@gmail.com>
2025-08-10 23:04:18 +0200

    fix: réorganisation des importations dans index.routes.ts pour une meilleure lisibilité

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import identityRoutes from './identity.routes';
-import destinyRoutes from './destiny.routes';
-import protocolRoutes from './protocol.routes';
-import healthRoutes from './health.routes';
-import statusRoutes from './status.routes';
+
 import { IdentityMiddleware } from '../middlewares/Identity.middleware';
+import destinyRoutes from './destiny.routes';
+import healthRoutes from './health.routes';
+import identityRoutes from './identity.routes';
+import protocolRoutes from './protocol.routes';
+import statusRoutes from './status.routes';
 
 const router = Router();
 
